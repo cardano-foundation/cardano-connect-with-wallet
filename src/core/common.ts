46c23a0a28331660242e16d2fecbf5b1df2ed162
jcaso <jcaso@protonmail.com>
2023-06-27 14:00:48 +0200

    chore: refactor chromeWalletExtensions info

--- src/core/common.ts
+++ src/core/common.ts
@@ -3,14 +3,41 @@ export enum NetworkId {
   TESTNET = 0,
 }
 
+export const mobileWallets = ['flint'];
+export const flintDeepLink = 'https://flint-wallet.app.link/browse?dappUrl=';
 export const chromeStoreUrl = 'https://chrome.google.com/webstore/detail/';
-export enum WalletExtensionId {
-  nami = 'lpfcbjknijpeeillifnkikgncikgfhdo',
-  flint = 'hnhobjmcibchnmglfbldbfabcgaknlkj',
-  typhon = 'kfdniefadaanbjodldohaedphafoffoh',
-  yoroi = 'ffnbelfdoeiohenkjibnmadjiehjhajb',
-  eternl = 'kmhcihpebfmpgmihbkipmjlmmioameka',
-  gerowallet = 'bgpipimickeadkjlklgciifhnalhdjhe',
-  nufi = 'gpnihlnnodeiiaakbikldcihojploeca',
-  lace = 'gafhhkghbfjjkeiendhlofajokpaflmk',
-}
+
+export const chromeWalletExtensions = {
+  NAMI: {
+    id: 'lpfcbjknijpeeillifnkikgncikgfhdo',
+    name: 'nami',
+  },
+  FLINT: {
+    id: 'hnhobjmcibchnmglfbldbfabcgaknlkj',
+    name: 'flint-wallet',
+  },
+  TYPHON: {
+    id: 'kfdniefadaanbjodldohaedphafoffoh',
+    name: 'typhon-wallet',
+  },
+  YOROI: {
+    id: 'ffnbelfdoeiohenkjibnmadjiehjhajb',
+    name: 'yoroi',
+  },
+  ETERNL: {
+    id: 'kmhcihpebfmpgmihbkipmjlmmioameka',
+    name: 'eternl',
+  },
+  GEROWALLET: {
+    id: 'bgpipimickeadkjlklgciifhnalhdjhe',
+    name: 'gerowallet',
+  },
+  NUFI: {
+    id: 'gpnihlnnodeiiaakbikldcihojploeca',
+    name: 'gerowallet',
+  },
+  LACE: {
+    id: 'gafhhkghbfjjkeiendhlofajokpaflmk',
+    name: 'lace',
+  },
+};
