404a86427b6e3d75f54a554d635adf3ed9311140
jcaso <jcaso@protonmail.com>
2023-06-27 13:37:47 +0200

    chore: refactor chromeStore constants

--- src/core/common.ts
+++ src/core/common.ts
@@ -2,3 +2,15 @@ export enum NetworkId {
   MAINNET = 1,
   TESTNET = 0,
 }
+
+export const chromeStoreUrl = 'https://chrome.google.com/webstore/detail/';
+export enum WalletExtensionId {
+  nami = 'lpfcbjknijpeeillifnkikgncikgfhdo',
+  flint = 'hnhobjmcibchnmglfbldbfabcgaknlkj',
+  typhon = 'kfdniefadaanbjodldohaedphafoffoh',
+  yoroi = 'ffnbelfdoeiohenkjibnmadjiehjhajb',
+  eternl = 'kmhcihpebfmpgmihbkipmjlmmioameka',
+  gerowallet = 'bgpipimickeadkjlklgciifhnalhdjhe',
+  nufi = 'gpnihlnnodeiiaakbikldcihojploeca',
+  lace = 'gafhhkghbfjjkeiendhlofajokpaflmk',
+}
