14e8aefe999735337cd601acf763ee336657a42e
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2024-03-13 13:31:54 +0100

    chore: add new rollup plugin to config file

--- react/rollup.config.js
+++ react/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import alias from 'rollup-plugin-alias';
+import alias from '@rollup/plugin-alias';
 
 const packageJson = require('./package.json');
 
