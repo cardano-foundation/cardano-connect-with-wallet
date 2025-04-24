932685deb4af6d8bf86649d57148a798fc771365
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2025-04-24 21:12:43 +0200

    feat: add new wallets and mobile wallets

--- core/utils/wallet.ts
+++ core/utils/wallet.ts
@@ -47,9 +47,32 @@ export enum NetworkId {
   TESTNET = 0,
 }
 
-export const mobileWallets = ['flint'];
+export const mobileWallets = ['flint', 'eternl', 'vespr', 'begin'];
 export const flintDeepLink = 'https://flint-wallet.app.link/browse?dappUrl=';
 export const chromeStoreUrl = 'https://chrome.google.com/webstore/detail/';
+
+export const nativeWallets = {
+  eternl: {
+    playStoreUrl:
+      'https://play.google.com/store/apps/details?id=art.nft_craze.gallery.main&utm_source=vespr_website',
+    appStoreUrl:
+      'https://apps.apple.com/de/app/eternl-by-tastenkunst/id1603854385',
+    walletName: 'Eternl',
+  },
+  vespr: {
+    playStoreUrl:
+      'https://play.google.com/store/apps/details?id=art.nft_craze.gallery.main&utm_source=vespr_website',
+    appStoreUrl:
+      'https://apps.apple.com/us/app/vespr-cardano-wallet/id1565749376',
+    walletName: 'Vespr',
+  },
+  begin: {
+    playStoreUrl: 'https://play.google.com/store/apps/details?id=is.begin.app',
+    appStoreUrl:
+      'https://apps.apple.com/us/app/begin-bitcoin-cardano-wallet/id1642488837',
+    walletName: 'Begin',
+  },
+};
 
 export const chromeWalletExtensions = {
   NAMI: {
@@ -84,6 +107,14 @@ export const chromeWalletExtensions = {
     id: 'gafhhkghbfjjkeiendhlofajokpaflmk',
     name: 'lace',
   },
+  BEGIN: {
+    id: 'nhbicdelgedinnbcidconlnfeionhbml',
+    name: 'begin',
+  },
+  VESPR: {
+    id: 'bedogdpgdnifilpgeianmmdabklhfkcn',
+    name: 'vespr',
+  },
 };
 
 const getWalletIcon = (walletName: string) => {
@@ -116,6 +147,14 @@ const getWalletIcon = (walletName: string) => {
     {
       name: 'peer-connect',
       icon: 'data:image/svg+xml,%3Csvg%20width%3D%22100%25%22%20height%3D%22100%25%22%20viewBox%3D%220%200%2033%2033%22%20version%3D%221.1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0A%20%20%20%20%3Cpath%20fill-rule%3D%22evenodd%22%20d%3D%22m22%2028h-2v1h2zm4%201v-1h2v1h1v-5h-1v-1h-1v3h-3v1h-1v1h2v-1h1v1h-1v1zm-7%200v-2h-1v1h-2v1zm-6-1h-1v1h3v-1h-1v-1h-1zm-2%201h-7v-7h7zm-6-6v5h5v-5zm4%204h-3v-3h3zm4-8h-2v1h-1v1h1v-1h1v6h2v1h1v-2h-2v-2h1v1h1v-1h1v-1h1v-1h-2v1h-2v-1h2v-1h-2zm8-1v-2h1v1h2v-1h4v1h1v-3h-1v1h-2v-1h2v-1h1v-1h-3v1h-1v-1h-1v1h-1v1h-1v-2h-1v2h-1v-2h-1v1h-2v-1h-1v1h-2v1h-1v2h1v-1h1v2h2v-2h1v-1h1v1h1v2h-2v2h3v1h-1v3h-1v-2h-1v2h-2v2h1v-1h1v2h-2v1h2v-1h1v1h1v-2h1v2h1v-1h1v-1h2v-5h-1v-1h1v1h1v2h2v-2h-1v-1h1v1h1v-2h-1v-1h-1v1h-2v-1h-1v2h-1v-1zm0%203v3h3v-3zm2%201h-1v1h1zm6%200h-1v1h1zm-22-2v-1h-1v2h2v-1zm11%200h-1v1h1zm-13-2v-2h-1v5h1v-2h1v-1zm5%201h-2v1h2zm7-1h-2v2h1v-1h1zm-6%200h-1v1h1zm4-1h-2v1h2zm-4-3v-1h-2v1zm1%203h-1v1h1zm-1-1h-1v1h1zm-4%200h-1v1h1zm-1-1h-1v1h1zm1-1h-1v1h1zm17%200v1h1v-1zm-19%200h-1v1h1zm1-1h-1v1h1zm-1-1h-1v1h1zm14%200v-3h-1v2h-1v1zm-1-4v-2h-4v1h-1v1h-1v1h2v2h-1v1h2v-4h1v-1h1v1zm-5%202h-1v1h1zm4%200h-1v1h1zm4%201v-6h-1v1h-1v1h1v4zm-10%200h-7v-7h7zm18%200h-7v-7h7zm-6-6v5h5v-5zm-18%200v5h5v-5zm12%203h-1v1h1zm10%201h-3v-3h3zm-18%200h-3v-3h3zm9-5h-6v3h1v-2h5zm1%201h-1v1h1zm-12%207v2h1v4h1v-2h1v-1h1v1h1v-2h1v-2z%22%2F%3E%0A%3C%2Fsvg%3E',
+    },
+    {
+      name: 'begin',
+      icon: "data:image/svg+xml,%3Csvg width='27' height='26' viewBox='0 0 27 26' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M23.7342 14.908C22.3376 16.3087 20.5093 17.1974 18.545 17.4301C18.5664 15.527 18.027 13.6597 16.9941 12.0611C17.5167 12.1382 18.05 12.0935 18.5524 11.9304C19.0548 11.7673 19.5127 11.4903 19.8903 11.121C21.2557 9.78667 21.2371 7.55941 19.8875 6.20976L17.4933 3.81562L21.3072 0L23.7328 2.42594C26.3191 5.01233 26.9648 8.80436 25.6695 11.9932C25.2261 13.0846 24.568 14.0758 23.7342 14.908Z' fill='%233414FC'/%3E%3Cpath d='M8.82646 22.1847L5.01263 26.0004L2.58704 23.5744C0.000656735 20.988 -0.645046 17.196 0.65029 14.0071C1.24517 12.548 2.21944 11.2744 3.47213 10.3185C4.72483 9.3625 6.21037 8.75894 7.77482 8.5703C7.75338 10.4734 8.29275 12.3407 9.32565 13.9392C8.8031 13.8621 8.26979 13.9068 7.76738 14.0699C7.26498 14.233 6.80708 14.5101 6.42946 14.8794C5.06408 16.2137 5.08266 18.4409 6.43232 19.7906L8.82646 22.1847Z' fill='%233414FC'/%3E%3Cpath d='M15.0677 23.5744L12.6418 26.0004L8.82652 22.1847L11.2521 19.7592C11.8954 19.1158 12.2568 18.2433 12.2568 17.3336C12.2568 16.4238 11.8954 15.5513 11.2521 14.908C9.59688 13.2528 8.66699 11.0078 8.66699 8.66696C8.66699 6.32613 9.59688 4.08117 11.2521 2.42594L13.678 0L17.4933 3.81562L15.0677 6.2412C14.4244 6.88452 14.0631 7.75702 14.0631 8.66679C14.0631 9.57655 14.4244 10.449 15.0677 11.0924C16.7229 12.7476 17.6528 14.9926 17.6528 17.3334C17.6528 19.6742 16.7229 21.9192 15.0677 23.5744Z' fill='%233414FC'/%3E%3C/svg%3E%0A",
+    },
+    {
+      name: 'vespr',
+      icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbDpzcGFjZT0icHJlc2VydmUiIHZpZXdCb3g9IjAgMCA2NDAgNjQwIj48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJyZ2JhKDksIDE0LCAyMiwgMSkiIHJ4PSIxMDAiLz48cGF0aCBmaWxsPSIjZmZmIiBkPSJtNDU1LjUxOCAxNzguMDQwOC0xMzUuMjQzNiAxODIuNzV2LS43Mzk2TDE4NC45NjIgMTc3LjE4MDhINzAuOTI2bDM1LjE3NCA0Ny41NThoMzMuOTE4NEwzMjAuMjA1NiA0NjIuMTE2di45NjMybDE4MC4yNTYtMjM3LjQ5NzZINTM0LjM4bDM1LjE3NC00Ny41NDA4SDQ1NS41MTh6Ii8+PHBhdGggZmlsbD0ibm9uZSIgc3Ryb2tlPSIjMDAwIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIHN0cm9rZS1taXRlcmxpbWl0PSIxMCIgZD0iTTMwMC45OTkgMTAyaC4wMDIiLz48L3N2Zz4=',
     },
   ];
 
