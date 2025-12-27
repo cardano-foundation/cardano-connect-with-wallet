340d6bbe40947b442ceaed489e067b145aef2c50
fabianbormann <bormann.fabian@gmail.com>
2025-12-27 20:05:30 +0000

    Deploying to gh-pages from @ cardano-foundation/cardano-connect-with-wallet@21c454bd833a8b09932b65263e708b7e695300ef 🚀

--- packages/react/storybook-static/components-ModalDialog-ModalDialog-stories.f55be763.iframe.bundle.js
+++ packages/react/storybook-static/components-ModalDialog-ModalDialog-stories.f55be763.iframe.bundle.js
@@ -1,17 +1,11 @@
 "use strict";(self.webpackChunk_cardano_foundation_cardano_connect_with_wallet=self.webpackChunk_cardano_foundation_cardano_connect_with_wallet||[]).push([[415],{"./src/components/ModalDialog/ModalDialog.stories.tsx":function(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{DefaultList:function(){return DefaultList}});var react__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("../node_modules/react/index.js"),_ModalDialog__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./src/components/ModalDialog/ModalDialog.tsx");__webpack_exports__.default={title:"Components/ModalDialog",component:_ModalDialog__WEBPACK_IMPORTED_MODULE_1__.A};const DefaultList=(args=>react__WEBPACK_IMPORTED_MODULE_0__.createElement(_ModalDialog__WEBPACK_IMPORTED_MODULE_1__.A,args)).bind({});DefaultList.args={content:"Hallo Hallo",visible:!0},DefaultList.parameters={...DefaultList.parameters,docs:{...DefaultList.parameters?.docs,source:{originalSource:"args => <ModalDialog {...args} />",...DefaultList.parameters?.docs?.source}}}},"./src/components/ModalDialog/ModalDialog.tsx":function(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.d(__webpack_exports__,{A:function(){return ModalDialog_ModalDialog}});var react=__webpack_require__("../node_modules/react/index.js"),styled_components_browser_esm=__webpack_require__("../node_modules/styled-components/dist/styled-components.browser.esm.js");const Modal=(0,styled_components_browser_esm.Ay)("div").withConfig({shouldForwardProp:prop=>!["customCSS"].includes(prop)})`
   position: fixed;
   z-index: 100;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  background-color: rgb(0, 0, 0);
+  inset: 0;
   background-color: rgba(0, 0, 0, 0.4);
   ${props=>props.customCSS}
 `,ModalContent=styled_components_browser_esm.Ay.div`
   background-color: #fefefe;
-  margin: 15% auto;
   padding: 20px;
   border: 1px solid #aeaeae;
   border-radius: 9px;
@@ -19,6 +13,20 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 101;
+  max-width: 90vw;
+  max-height: 90vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  box-shadow: 0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2);
+
+  &:focus {
+    outline: none;
+  }
 `,CopyInputContainer=(0,styled_components_browser_esm.Ay)("div").withConfig({shouldForwardProp:prop=>!["primaryColor"].includes(prop)})`
   display: flex;
   align-items: center;
@@ -68,5 +76,5 @@
     visibility: visible;
     animation: ${fadeInOut} 3s ease;
   }
-`;var dist=__webpack_require__("../node_modules/react-qrcode-logo/dist/index.js");var ModalDialog_ModalDialog=props=>{const{visible:visible,content:content,handleClose:handleClose,qrStyle:qrStyle,eyeRadius:eyeRadius,icon:icon,subtitle:subtitle,primaryColor:primaryColor,customCSS:customCSS}=props,toastRef=(0,react.useRef)(null);return react.createElement(Modal,{onClick:()=>{"function"==typeof handleClose&&handleClose()},customCSS:customCSS||"",style:{display:visible?"block":"none"}},react.createElement(ModalContent,{"data-testid":"modal-content"},react.createElement(dist.QRCode,{removeQrCodeBehindLogo:!0,logoImage:icon||void 0,ecLevel:"H",eyeRadius:eyeRadius||0,qrStyle:qrStyle||"squares",value:content}),react.createElement(CopyInputContainer,{primaryColor:primaryColor||"#0538AF"},react.createElement("input",{type:"text",value:content,style:{textOverflow:"ellipsis"},readOnly:!0}),react.createElement("button",{onClick:event=>{event.stopPropagation(),navigator.clipboard.writeText(content),toastRef.current?.classList.add("show"),setTimeout((function(){toastRef.current?.classList.remove("show")}),3e3)}},"Copy")),react.createElement("p",{style:{maxWidth:300}},subtitle||"You can utilize a CIP45-compliant mobile wallet, such as Eternl, on your smartphone to establish a connection as a peer-to-peer wallet."),react.createElement(Toast,{ref:toastRef},"P2P identifier copied to clipboard!")))}}}]);
-//# sourceMappingURL=components-ModalDialog-ModalDialog-stories.b6f83a98.iframe.bundle.js.map
+`;var dist=__webpack_require__("../node_modules/react-qrcode-logo/dist/index.js"),react_dialog_dist=__webpack_require__("../node_modules/@radix-ui/react-dialog/dist/index.mjs"),react_visually_hidden_dist=__webpack_require__("../node_modules/@radix-ui/react-visually-hidden/dist/index.mjs");var ModalDialog_ModalDialog=props=>{const{visible:visible,content:content,handleClose:handleClose,qrStyle:qrStyle,eyeRadius:eyeRadius,icon:icon,subtitle:subtitle,primaryColor:primaryColor,customCSS:customCSS}=props,toastRef=(0,react.useRef)(null);return react.createElement(react_dialog_dist.bL,{open:visible,onOpenChange:open=>!open&&handleClose()},react.createElement(react_dialog_dist.ZL,null,react.createElement(react_dialog_dist.hJ,{asChild:!0},react.createElement(Modal,{customCSS:customCSS||""})),react.createElement(react_dialog_dist.UC,{asChild:!0},react.createElement(ModalContent,{"data-testid":"modal-content"},react.createElement(react_visually_hidden_dist.s6,null,react.createElement(react_dialog_dist.hE,null,"P2P Wallet Connection")),react.createElement(dist.QRCode,{removeQrCodeBehindLogo:!0,logoImage:icon||void 0,ecLevel:"H",eyeRadius:eyeRadius||0,qrStyle:qrStyle||"squares",value:content}),react.createElement(CopyInputContainer,{primaryColor:primaryColor||"#0538AF"},react.createElement("input",{type:"text",value:content,style:{textOverflow:"ellipsis"},readOnly:!0}),react.createElement("button",{onClick:event=>{event.stopPropagation(),navigator.clipboard.writeText(content),toastRef.current?.classList.add("show"),setTimeout((function(){toastRef.current?.classList.remove("show")}),3e3)}},"Copy")),react.createElement(react_dialog_dist.VY,{asChild:!0},react.createElement("p",{style:{maxWidth:300}},subtitle||"You can utilize a CIP45-compliant mobile wallet, such as Eternl, on your smartphone to establish a connection as a peer-to-peer wallet.")),react.createElement(Toast,{ref:toastRef},"P2P identifier copied to clipboard!")))))}}}]);
+//# sourceMappingURL=components-ModalDialog-ModalDialog-stories.f55be763.iframe.bundle.js.map
